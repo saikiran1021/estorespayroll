043e7857b8fe7119d25bdb1a905511e5f778ebd0
eStores <info.estoresglobal@gmail.com>
2025-12-06 16:16:57 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-670735781-4e9b0",
+  "appId": "1:1023485576308:web:d190ee08957ed63712fd21",
+  "apiKey": "AIzaSyA84E9BIuHEsUeJXZbf6ZeBgbuSoOTfzeY",
+  "authDomain": "studio-670735781-4e9b0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1023485576308"
+};
