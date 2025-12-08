b59d6c765f4f234eb77cf19935fe2b52900994e7
eStores <info.estoresglobal@gmail.com>
2025-12-08 09:49:14 +0000

    ## Error Type Console SyntaxError
    ## Error Message
    Unexpected token '<'

--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -1,5 +1,3 @@
-'use server';
-
 import { initializeApp, cert, getApps, App } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';
